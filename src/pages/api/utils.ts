a065bbc58a759451c81b2e39cf0b2ba3f20ac853
Sayuru Akash Amarasinghe <sayuru555@gmail.com>
2023-04-06 23:28:52 +0530

    API updates

---
+++ src/pages/api/utils.ts
@@ -0,0 +1,23 @@
+import { withIronSession } from "next-iron-session";
+import contract from "../../../public/contracts/NFTMarket.json";
+
+const NETWORKS = {
+  "5777": "Ganache",
+};
+
+type NETWORK = typeof NETWORKS;
+
+const targetNetwork = process.env.NEXT_PUBLIC_NETWORK_ID as keyof NETWORK;
+
+// @ts-ignore
+export const contractAddress = contract["networks"][targetNetwork]["address"];
+
+export function withSession(handler: any) {
+  return withIronSession(handler, {
+    password: process.env.SECRET_COOKIE_PASSWORD as string,
+    cookieName: "nft-auth-session",
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production" ? true : false,
+    },
+  });
+}
